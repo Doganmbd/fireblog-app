af2cb4d71681a5166eb83950c132da94bb25d975
DMB29 <mbilaldogan90@gmail.com>
2022-05-13 09:28:16 +0300

    firebase

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -3,15 +3,22 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
 // Your web app's Firebase configuration
-const firebaseConfig = {
+const app =initializeApp ({
   apiKey:process.env.REACT_APP_FİREBASE_apiKey,
   authDomain:process.env.REACT_APP_FİREBASE_authDomain,
+  databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
   projectId:process.env.REACT_APP_FİREBASE_projectId,
   storageBucket: process.env.REACT_APP_FİREBASE_storageBucket,
   messagingSenderId:process.env.REACT_APP_FİREBASE_messagingSenderId,
   appId: process.env.REACT_APP_FİREBASE_appId
-};
+});
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
